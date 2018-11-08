cf9141f89dd0e74955d5f7e859fc59c72cd068a8
Matthew King <mattking73@gmail.com>
2018-11-08 15:27:57 -0800

    Update to be a 4x4 array grid

--- pages/game.js
+++ pages/game.js
@@ -4,22 +4,29 @@ import css from '../styles/game.scss'
 export default class Game extends Component {
   state = {
     score: 0,
-    grid: [0,0,0,0,0,0,0,0,0,0,4,2,0,0,0,0]
+    grid: [
+      [0,0,0,0],
+      [0,0,0,0],
+      [0,0,4,2],
+      [0,0,0,0]
+    ]
   }
   render() {
-    const tiles = this.state.grid.map((n, i) => {
-      if (n === 0) {
-        return <div className={css.emptyTile} key={i}></div>
-      }
-
-      const tileClass = css['tile' + n];
-      return <div className={tileClass} key={i}>
-        <div className={css.number}>{n}</div>
-      </div>
-    })
+    const tiles = this.state.grid.map((row) => row.map(getTile));
 
     return (
       <div className={css.game}>{tiles}</div>
     )
   }
+}
+
+function getTile(n) {
+  if (n === 0) {
+    return <div className={css.emptyTile} key={n}></div>
+  }
+
+  const tileClass = css['tile' + n];
+  return <div className={tileClass} key={n}>
+    <div className={css.number}>{n}</div>
+  </div>
 }
