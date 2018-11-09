ee36737597eb70093d73e02f6d6a5b2a832cf0f2
Matthew King <mattking73@gmail.com>
2018-11-09 09:52:56 -0800

    Fix link to use actual react link component with separate override

--- components/header.js
+++ components/header.js
@@ -1,9 +1,11 @@
 import css from '../styles/site.scss';
-import Link from 'next/link';
+import Link from './link';
 
 const Header = () => (
   <div className={css.header}>
-    <a href="/" as={`${process.env.ASSET_PREFIX}/`} className={css.home}>2048</a>
+    <Link href="/">
+      <a>2048</a>
+    </Link>
   </div>
 );
 
