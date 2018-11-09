a1b6c786c3a1742c49bdefed3465e2a4ed77a05c
Matthew King <mattking73@gmail.com>
2018-11-08 20:48:20 -0800

    Add page title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Layout from '../components/layout'
 import Game from './game'
+import Head from 'next/head'
 import css from '../styles/site.scss'
 
 export default () => (
   <Layout>
+    <Head>
+      <title>2048</title>
+    </Head>
     <div className={css.title}>
       <h1>2048</h1>
     </div>
