c99e8ed22a6faccb9d7243af6b4e83aa61c07e5c
Matthew King <mattking73@gmail.com>
2018-11-08 19:59:39 -0800

    Update scripts for asset/production use

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   cssModules: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/2048' : '',
   exportPathMap: () => ({
     '/': { page: '/' }
-  }) ,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : '',
+  })
 })
