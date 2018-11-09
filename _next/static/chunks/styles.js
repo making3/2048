91d1401110137efc0a003a95e5b2867cf8f275d1
Matthew King <mattking73@gmail.com>
2018-11-09 09:41:48 -0800

    Fix page-prefix link, re-export

---
+++ _next/static/chunks/styles.js
@@ -0,0 +1,80 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+
+/***/ "./styles/game.scss":
+/*!**************************!*\
+  !*** ./styles/game.scss ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"header":"_3N-leQeneH2Zcccp9UebWn","home":"_1aTeJ66qRAFIpC2NgCx6et","layout":"_2In20ubZVjFrucPxZRUHLH","title":"_6l23jZzOo6q55YFMCf1p-","score":"_1oSEI3hCeF9t0q-Tfizock","label":"_1SGF4ISJzEa4aMAUYTKHbr","game":"_1YL6naGd0nFD3O76JXj3DX","emptyTile":"LvsULW60rD1YBsyDb_Djf","tile2":"NszF2S6wu0mq4abz2Kjpn","tile4":"_9jMboZCVdFQihqx3mTiIN","tile8":"-UwE-2avbm8RxqnOTeJut","tile16":"_1rkpkKxl1iwbJEgQfgqY9W","tile32":"_1OkzKuOjbZuzL1OYgb84Hl","tile64":"_10iCZWHMcH0s7NZKNqt_8t","tile128":"_30JYxHNmTlSrcCw6hu8zgz","tile256":"_3V-aQoARG44Y1PJYn1xAGt","tile512":"_1utp-0nv8sVYYeaHXb_TyM","tile1024":"bJLAxHHhzRvKzYLHjWKtS","tile2048":"jUDRHDv5VVSkzZhlNPIi","number":"_3QKQNUb7c97mJnTYskWsqM"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1541785183540");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./styles/site.scss":
+/*!**************************!*\
+  !*** ./styles/site.scss ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"header":"_2fwUbvtTqcMY-6VfkJeI3_","home":"_10pxxkj_CPVVX1GK0agd38","layout":"w9EqiR3fwLZqICekFSI1C","title":"_2DisoATwn5se4uyObVwDXW"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1541785183395");
+          });
+      }
+    }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=styles.js.map
